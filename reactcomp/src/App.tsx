0b1c942c36da658090be37bbd89c8853ad431636
hazekost <kostyk0007@gmail.com>
2020-12-21 13:57:33 +0300

    add component Rating, Star, Accordion in App.tsx

--- reactcomp/src/App.tsx
+++ reactcomp/src/App.tsx
@@ -2,9 +2,42 @@ import React from 'react';
 import './App.css';
 
 export function App() {
-  return (
-    <div>
-      This is App component
-    </div>
-  );
+    return (
+        <div>
+            This is App component
+            <Rating/>
+            <Accordion/>
+        </div>
+    );
+}
+
+function Rating() {
+    return (
+        <div>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+        </div>
+    )
+}
+
+function Star() {
+    return (
+        <div>Star</div>
+    )
+}
+
+function Accordion() {
+    return (
+        <div>
+            <h3>Menu</h3>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </div>
+    )
 }
